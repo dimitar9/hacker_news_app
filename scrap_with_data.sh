c682cf2d4a464b12de92d822423ff606bdd1aaad
dimitar9 <dimitar9909@gmail.com>
2014-02-10 00:26:44 -0500

    modify scrap data script. Remove news everytime

--- scrap_with_data.sh
+++ scrap_with_data.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 rm news.20*
-#curl http://api.ihackernews.com/page > news
-curl http://api.ihackernews.com/new > news
-curl http://hnify.herokuapp.com/get/newest > hnify_news_newest
+rm news
+curl http://api.ihackernews.com/page > news
+#curl http://api.ihackernews.com/new > news
+#curl http://hnify.herokuapp.com/get/newest > hnify_news_newest
 filename="news"
 echo $filename 
 current_time=$(date "+%Y.%m.%d-%H.%M.%S")
