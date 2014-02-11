a4b8f6464a8ca7000a7adfdc1c259a4407a7b103
dimitar9 <dimitar9909@gmail.com>
2014-02-11 01:26:54 -0500

    add retry to cron_run script

--- cron_run.sh
+++ cron_run.sh
@@ -7,9 +7,21 @@ substring="items"
 if [[ $my_string == *"$substring"* ]];
 then
 	echo "yes,sucessfully got JSON content from hacker news unofficial API"  > /home/paul/Documents/linuxwork/hackerNewsApp/gotjson
-    cd /home/paul/Documents/linuxwork/hackerNewsApp/	
-    /home/paul/.rvm/rubies/ruby-2.0.0-p353/bin/ruby /home/paul/Documents/linuxwork/hackerNewsApp/db.rb 
+    	cd /home/paul/Documents/linuxwork/hackerNewsApp/	
+    	/home/paul/.rvm/rubies/ruby-2.0.0-p353/bin/ruby /home/paul/Documents/linuxwork/hackerNewsApp/db.rb 
 	python /home/paul/Documents/linuxwork/hackerNewsApp/db_py.py
+else
+	echo "retry to get json"  > /home/paul/Documents/linuxwork/hackerNewsApp/gotjson
+	sh /home/paul/Documents/linuxwork/hackerNewsApp/scrap_with_data.sh
+	if [[ $my_string2 == *"$substring"* ]];
+	then
+    		cd /home/paul/Documents/linuxwork/hackerNewsApp/	
+    		/home/paul/.rvm/rubies/ruby-2.0.0-p353/bin/ruby /home/paul/Documents/linuxwork/hackerNewsApp/db.rb 
+		python /home/paul/Documents/linuxwork/hackerNewsApp/db_py.py
+		echo "retry sucess"  > /home/paul/Documents/linuxwork/hackerNewsApp/gotjson
+	else
+		echo "retry failed"  > /home/paul/Documents/linuxwork/hackerNewsApp/gotjson
+	fi
 fi
 
 
